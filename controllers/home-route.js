f4b329f4c4bc42433a73fc3b3c902e171eb24228
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-06-29 03:49:13 -0500

    pushing to heroku

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -89,7 +89,7 @@ router.get("/login", (req, res) => {
     res.redirect("/");
     return;
   }
-  // ! This is making sure that the login button is changed to logout if the user is logged in
+  // This is making sure that the login button is changed to logout if the user is logged in
   res.render("login", { loggedIn: req.session.loggedIn });
 });
 
