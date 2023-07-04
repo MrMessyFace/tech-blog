82dab14ef4f9d4c1e2150824d7265531a5248652
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-07-04 16:24:40 -0500

    update

--- config/config.js
+++ config/config.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize');
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 
 module.exports = sequelize;
